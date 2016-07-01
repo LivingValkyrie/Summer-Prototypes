67b6b385d395f9db6284f69a6feb6ab8c1571d1f
LivingValkyrie <deadwynn@gmail.com>
2016-06-30 22:42:42 -0500

    raycast implemented
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

--- Resource gathering prototype/Assets/Player.cs
+++ Resource gathering prototype/Assets/Player.cs
@@ -10,23 +10,32 @@ using System.Collections.Generic;
 /// Description: Player 
 /// </summary>
 public class Player : MonoBehaviour {
-
 	#region Fields
 
 	public float moveSpeed = 10;
 	public float rotSpeed = 5;
+	public float rayLength = 5;
 
 	#endregion
-	
-	void Update () {
 
+	void Update() {
+		Move();
+
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, rayLength)) {
+				print(hit.transform.name);
+			}
+		}
+	}
+
+	void Move() {
 		float rotation = Input.GetAxis("Horizontal");
 		float vspeed = Input.GetAxis("Vertical");
 
-		Vector3 velocity = new Vector3(0, 0, vspeed); 
+		Vector3 velocity = new Vector3(0, 0, vspeed);
 
-		transform.Translate( velocity * moveSpeed * Time.deltaTime);
+		transform.Translate(velocity * moveSpeed * Time.deltaTime);
 		transform.Rotate(0, rotation * rotSpeed, 0);
-
 	}
 }
